51b2912eb5f7830c30186e97db187e5fb228c19a
Noah Lundquist <nalundquist@gmail.com>
2022-09-18 12:58:26 -0700

    add static function, refactor and separate code

--- src/currex.js
+++ src/currex.js
@@ -1,18 +1,7 @@
-import {errorReturn} from './index.js';
-// import {currCalc} from './currcalc.js';
-import {currDisplay} from './index.js';
-
 export class CurrEx {
-
-	constructor(amount, currencyFrom, currencyTo){
-		this.convertAmt = amount;
-		this.convertFrom = currencyFrom;
-		this.convertTo = currencyTo;
-	}
-	apiGet() {
-		const convertFrom = this.convertFrom
-		let currPromise = new Promise(function(resolve, reject){
-			let currRequest = new XMLHttpRequest;
+	static apiGet(convertFrom) {
+		let promise = new Promise(function(resolve, reject){
+			let currRequest = new XMLHttpRequest();
 			const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${convertFrom}`
 			currRequest.addEventListener("loadend", function(){
 				const currResponse = JSON.parse(this.responseText);
@@ -26,39 +15,6 @@ export class CurrEx {
 			currRequest.open("GET", url, true);
 			currRequest.send();
 		})
-		currPromise.then((response) => {
-			this.convertMult = 0;
-			this.convertFinal = 0;
-			this.errorMessage = "";
-			if (this.convertTo === "USD"){
-				this.convertMult = response.conversion_rates.USD;
-			} else if (this.convertTo === "TND"){
-				this.convertMult = response.conversion_rates.TNR;
-			} else if (this.convertTo === "OMR"){
-				this.convertMult = response.conversion_rates.OMR;
-			} else if (this.convertTo === "NPR"){
-				this.convertMult = response.conversion_rates.NPR;
-			} else if (this.convertTo === "STN"){
-				this.convertMult = response.conversion_rates.STN;
-			} else if (this.convertTo === "RUB"){
-				this.convertMult = response.conversion_rates.RUB;
-			} else if (this.convertTo === "SEK"){
-				this.convertMult = response.conversion_rates.SEK;
-			} else if (this.convertTo === "HTG"){
-				this.convertMult = response.conversion_rates.HTG;
-			} else if (this.convertTo === "CLP"){
-				this.convertMult = response.conversion_rates.CLP;
-			} else if (this.convertTo === "KPW"){
-				this.errorMessage = `North Korean Won is an unsupported currency at this time, apologies for the inconvenience.`;
-			} else {
-				this.errorMessage = `Error: Unsupported Currency`
-			}
-			this.convertFinal = (this.convertAmt * this.convertMult)
-			currDisplay(this.convertTo, this.convertFrom, this.convertAmt, this.convertFinal, this.errorMessage);
-		}, (errorMessage) => {
-			errorReturn(errorMessage);
-		});
+		return promise
 	}
-
-
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,45 @@
 import './css/styles.css';
 import { CurrEx } from './currex.js';
 
-export const errorReturn = (errorMessage) => {
+//Business Logic
+
+const apiGet = (amt, convertTo, convertFrom) => {
+	let promise = CurrEx.apiGet(convertFrom);
+	promise.then((response) => {
+		currConvert(response, amt, convertTo, convertFrom);
+	}, (errorMessage) => {
+		errorReturn2(errorMessage);
+	});
+}
+
+const currConvert = (response, amt, convertTo, convertFrom) => {
+	let convertMult = 0;
+	let convertFinal = 0;
+	let errorMessage = "";
+	console.log(convertTo)
+	console.log(convertTo === "KPW")
+	if (convertFrom === null || convertTo === null || convertFrom === "KPW" || convertTo === "KPW"){
+		errorMessage = `Error: Unsupported Currency`;
+		errorReturn1(errorMessage)
+	} else {
+		convertMult = response.conversion_rates[convertTo]
+		convertFinal = (amt * convertMult);
+		currDisplay(convertTo, convertFrom, amt, convertFinal);
+	}
+}
+
+//UI Logic
+
+const errorReturn1 = (error) => {
+	const convResult = document.getElementById("convert-result");
+	const h3 = document.createElement("h3");
+	convResult.append(h3);
+	h3.append(error);
+	convResult.setAttribute("class", "result-display");
+	convResult.removeAttribute("class", "hidden")
+}
+
+const errorReturn2 = (errorMessage) => {
 	const convResult = document.getElementById("convert-result");
 	convResult.innerHTML = null
 	const h4 = document.createElement("h4");
@@ -11,15 +49,7 @@ export const errorReturn = (errorMessage) => {
 	convResult.removeAttribute("class", "hidden")
 }
 
-export const currDisplay = (to, from, amt, final, error) => {
-	const convResult = document.getElementById("convert-result");
-	if (error != ""){
-		const h3 = document.createElement("h3");
-		convResult.append(h3);
-		h3.append(error);
-		convResult.setAttribute("class", "result-display");
-		convResult.removeAttribute("class", "hidden")
-	}	else {
+const currDisplay = (to, from, amt, final) => {
 	const convResult = document.getElementById("convert-result");
 	convResult.innerHTML = null;
 	const h2 = document.createElement("h2");
@@ -30,7 +60,6 @@ export const currDisplay = (to, from, amt, final, error) => {
 	h4.append(`is the equivalent of ${amt} ${from}`);
 	convResult.setAttribute("class", "result-display");
 	convResult.removeAttribute("class", "hidden")
-	}
 }
 
 const formSubmit = (event) => {
@@ -45,8 +74,7 @@ const formSubmit = (event) => {
 	document.getElementById("convert-from").value = "USD";
 	document.getElementById("convert-to").value = "USD";
 	document.getElementById("convert-amt").value = 0.00;
-	let currEx = new CurrEx(convertAmt, convertFrom, convertTo);
-	currEx.apiGet();
+	apiGet(convertAmt, convertTo, convertFrom);
 }
 
 window.addEventListener("load", function(){
